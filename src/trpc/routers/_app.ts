01a5ae0eb5195256dc001f80d870d865e9ee4833
omarwaness <o5v4w2000@gmail.com>
2026-04-07 20:22:51 +0100

    06-background-jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,21 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { inngest } from '@/inngest/client';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany();
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
   }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "code@example.com"
+      },
+    })
+
+    return { success: true, message: "Job queued successfully" }
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
