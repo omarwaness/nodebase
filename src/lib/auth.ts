99f35a1d409a0af4e5057b0896ccc7941e12848e
omarwaness <o5v4w2000@gmail.com>
2026-04-11 20:13:25 +0100

    10-payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
+import { polar, checkout, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
+import { polarClient } from "./polar";
+import { tr } from "date-fns/locale";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +13,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "de82a072-8bda-4cb2-9bfb-87bab0740007",
+              slug: "pro",
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal()
+      ]
+    })
+  ]
 });
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,32 +1,29 @@
 import { inngest } from '@/inngest/client';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
-
+import { TRPCError } from '@trpc/server';
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
+  testAi: premiumProcedure.mutation(async () => {
     await inngest.send({
       name: "execute/ai",
-    })
+    });
 
     return { success: true, message: "Job queued successfully" }
   }),
-
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
-
   createWorkflow: protectedProcedure.mutation(async () => {
-
     await inngest.send({
       name: "test/hello.world",
       data: {
-        email: "code@example.com"
+        email: "code@mail.com",
       },
-    })
+    });
 
     return { success: true, message: "Job queued successfully" }
-  })
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
