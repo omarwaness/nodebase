fdc9ff5e864eb037d474721ac91c6a17386ae81a
omarwaness <o5v4w2000@gmail.com>
2026-04-12 20:00:22 +0100

    15-editor

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -3,13 +3,23 @@ import prisma from "@/lib/db";
 import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import z from 'zod';
 import { PAGINATION } from '@/config/constants';
+import { NodeType } from '@/generated/prisma/wasm';
+import type { Node, Edge } from '@xyflow/react';
+import { connection } from 'next/server';
 
 export const workflowsRouter = createTRPCRouter({
     create: premiumProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
             data: {
                 name: generateSlug(3),
-                userId: ctx.auth.user.id
+                userId: ctx.auth.user.id,
+                nodes: {
+                    create: {
+                        type: NodeType.INITIAL,
+                        position: { x: 0, y: 0 },
+                        name: NodeType.INITIAL
+                    },
+                }
             }
         })
     }),
@@ -36,10 +46,33 @@ export const workflowsRouter = createTRPCRouter({
 
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
-        .query(({ ctx, input }) => {
-            return prisma.workflow.findUniqueOrThrow({
-                where: { id: input.id, userId: ctx.auth.user.id }
+        .query(async ({ ctx, input }) => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: { id: input.id, userId: ctx.auth.user.id },
+                include: { nodes: true, connections: true }
             })
+
+            const nodes: Node[] = workflow.nodes.map((node) => ({
+                id: node.id,
+                type: node.type,
+                position: node.position as { x: number, y: number },
+                data: (node.data as Record<string, unknown>) || {},
+            }))
+
+            const edges: Edge[] = workflow.connections.map((connection) => ({
+                id: connection.id,
+                source: connection.fromNodeId,
+                target: connection.toNodeId,
+                sourceHandle: connection.fromOutput,
+                targetHandle: connection.toInput,
+            }))
+
+            return {
+                id: workflow.id,
+                name: workflow.name,
+                nodes,
+                edges
+            }
         }),
 
     getMany: protectedProcedure
