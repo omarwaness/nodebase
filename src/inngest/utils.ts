5e113bc7c9c8887fb06e0670eedeb260de92c330
omarwaness <o5v4w2000@gmail.com>
2026-04-15 19:26:43 +0100

    18-node-execution

---
+++ src/inngest/utils.ts
@@ -0,0 +1,49 @@
+import { Connection, Node } from "@/generated/prisma";
+import toposort from "toposort";
+import { boolean } from "zod";
+
+export const topologicalSort = (
+    nodes: Node[],
+    connections: Connection[]
+): Node[] => {
+    // if no connections, return nodes as is
+    if (connections.length === 0) {
+        return nodes;
+    }
+
+    // create edges for toposort
+    const edges: [string, string][] = connections.map((conn) => [
+        conn.fromNodeId,
+        conn.toNodeId,
+    ]);
+
+    // add nodes without connections to edges to ensure they are included
+    const connectedNodeIds = new Set<string>();
+    for (const conn of connections) {
+        connectedNodeIds.add(conn.fromNodeId);
+        connectedNodeIds.add(conn.toNodeId);
+    }
+
+    for (const node of nodes) {
+        if (!connectedNodeIds.has(node.id)) {
+            edges.push([node.id, node.id]);
+        }
+    }
+
+    // perform topological sort
+    let sortedNodeIds: string[];
+    try {
+        sortedNodeIds = toposort(edges);
+        // remove duplicates  from self edges
+        sortedNodeIds = [...new Set(sortedNodeIds)];
+    } catch (error) {
+        if (error instanceof Error && error.message.includes("Cyclic")) {
+            throw new Error("Workflow contains a cycle");
+        }
+        throw error;
+    }
+
+    // map sorted node IDs back to nodes objects
+    const nodeMap = new Map(nodes.map((node) => [node.id, node]));
+    return sortedNodeIds.map((id) => nodeMap.get(id)!).filter(boolean);
+}
