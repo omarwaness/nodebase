52fd5deab9040c0581a23e5c99d1e90835c06eda
omarwaness <o5v4w2000@gmail.com>
2026-05-01 17:48:33 +0100

    28-encryption

--- src/features/executions/components/openai/executor.ts
+++ src/features/executions/components/openai/executor.ts
@@ -5,6 +5,7 @@ import { createOpenAI } from "@ai-sdk/openai";
 import type { NodeExecutor } from "@/features/executions/types";
 import { openAiChannel } from "@/inngest/channels/openai";
 import prisma from "@/lib/db";
+import { decrypt } from "@/lib/encryption";
 
 Handlebars.registerHelper("json", (context) => {
   const jsonString = JSON.stringify(context, null, 2);
@@ -51,7 +52,7 @@ export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
         status: "error",
       }),
     );
-    throw new NonRetriableError("Gemini node: Credential is required");
+    throw new NonRetriableError("OpenAi node: Credential is required");
   }
 
   if (!data.userPrompt) {
@@ -82,7 +83,7 @@ export const openAiExecutor: NodeExecutor<OpenAiData> = async ({
   }
 
   const openai = createOpenAI({
-    apiKey: credential.value,
+    apiKey: decrypt(credential.value),
   });
 
   try {
